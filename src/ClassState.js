0d68007d2f0e367c5fa6a7817741a3d4d9475055
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-10 12:24:01 -0500

    EstadosSimples:_React.Component_vs_React.useState
    Clase 3

--- src/ClassState.js
+++ src/ClassState.js
@@ -2,13 +2,28 @@ import React from "react";
 
 
 class ClassState extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            error: true,
+        };
+    }
+
+
     render(){
         return (
+            
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escribe el codigo de seguridad</p>
+                {this.state.error &&(
+                    <p>Error: El codigo es incorrecto</p>
+                )}
                 <input placeholder="Codigo de seguridad"/>
-                <button>Comprobar</button>
+                <button
+                    //onClick={()=>this.setState({error: !this.state.error})}
+                    onClick={()=>this.setState(prevState=> ({error: !prevState.error})) }
+                >Comprobar</button>
             </div>
         );
     }
