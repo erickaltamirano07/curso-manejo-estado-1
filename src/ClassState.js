7b9b10ad3bdb33eb01adf36068c6eb9a404a9809
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-10 21:20:20 -0500

    Estados compuestos con React.Component
    Estados compuestos

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = 'paradigma';
 
 class ClassState extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            error: true,
+        this.state = {
+            value: "",
+            error: false,
             loading: false
         };
     }
@@ -26,27 +28,38 @@ class ClassState extends React.Component{
             setTimeout(()=>{
                 console.log("Haciendo la validacion");
 
-                this.setState({loading: false});
+                if (SECURITY_CODE === this.state.value) {
+                    this.setState({error:false, loading: false });
+                } else {
+                    this.setState({ error: true, loading: false });
+                }                
 
                 console.log("Terminado la validacion");
             },3000);
         }   
     }
 
-    render(){
+    render() {
+//        const { error, loading, value } = this.state;
         return (
             
             <div>
                 <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escribe el codigo de seguridad</p>
-                {this.state.error &&(
+                {(this.state.error&&!this.state.loading )&&(
                     <p>Error: El codigo es incorrecto</p>
                 )}
 
                 {this.state.loading &&(
                     <Loading/>
                 )}
-                <input placeholder="Codigo de seguridad"/>
+                <input
+                    placeholder="Codigo de seguridad"
+                    value={this.state.value}
+                    onChange={(event) => {
+                        this.setState({ value: event.target.value });                                            
+                    }}
+                />
                 <button
                     //onClick={()=>this.setState({error: !this.state.error})}
                     onClick={()=>this.setState({loading: true})}
